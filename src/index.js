273db3ad522dc5b55c3306769f6b543a40f4be92
Lucas Feitosa Alves <lukasalves271@gmail.com>
2023-05-17 12:08:11 -0300

    resolve conflicts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 const express = require('express');
+const errorHandler = require('./app/middlewares/errorHandler');
 require('express-async-errors');
 
 const router = require('./routes');
 
 const app = express();
-
 app.use(express.json());
 app.use(router);
-
-app.use((error, req, res, next) => {
-  console.log('### Error Handle ###');
-  console.log(error);
-  res.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3000, () => console.log('app listening in http://localhost:3000'));
